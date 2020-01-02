a70a108f0173e867ca2fb230e21c937975254158
Marshall Greenblatt <magreenblatt@gmail.com>
2020-01-02 13:54:23 +0200

    Fix gsutil UserWarning: Unable to load AWS_CREDENTIAL_FILE (see issue #338)

--- tools/buildtools/download_from_google_storage.py
+++ tools/buildtools/download_from_google_storage.py
@@ -60,7 +60,7 @@ class Gsutil(object):
   RETRY_BASE_DELAY = 5.0
   RETRY_DELAY_MULTIPLE = 1.3
 
-  def __init__(self, path, boto_path=None, timeout=None, version='4.15'):
+  def __init__(self, path, boto_path=None, timeout=None, version='4.46'):
     if not os.path.exists(path):
       raise FileNotFoundError('GSUtil not found in %s' % path)
     self.path = path
