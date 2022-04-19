194c64a2a1578db1e055086ee667650688b62be8
apique <apique@users.noreply.github.com>
2022-04-19 15:01:37 +0200

    Prevent JNI native reference leak on jrouterhandler when removing handler
    The JNI object is retained in the "addHandler" method but was never released.

--- native/CefMessageRouter_N.cpp
+++ native/CefMessageRouter_N.cpp
@@ -109,6 +109,11 @@ Java_org_cef_browser_CefMessageRouter_1N_N_1RemoveHandler(
                             },
                             msgRouter, routerHandler));
   }
+  
+  // Remove JNI reference on jrouterhandler added by the ScopedJNIObject
+  SetCefForJNIObject<MessageRouterHandler>(env, jrouterHandler, nullptr,
+                                           "CefMessageRouterHandler");
+  
   return JNI_TRUE;
 }
 
