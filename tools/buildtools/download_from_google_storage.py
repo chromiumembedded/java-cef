94d34344bff84fca353c2494d70f7bbf72e6f762
ds58 <30220598+ds58@users.noreply.github.com>
2022-04-27 21:36:12 -0500

    Fix building on MacOS

--- tools/buildtools/download_from_google_storage.py
+++ tools/buildtools/download_from_google_storage.py
@@ -78,7 +78,7 @@ class Gsutil(object):
   RETRY_BASE_DELAY = 5.0
   RETRY_DELAY_MULTIPLE = 1.3
 
-  def __init__(self, path, boto_path=None, version='4.46'):
+  def __init__(self, path, boto_path=None, version='5.9'):
     if not os.path.exists(path):
       raise FileNotFoundError('GSUtil not found in %s' % path)
     self.path = path
