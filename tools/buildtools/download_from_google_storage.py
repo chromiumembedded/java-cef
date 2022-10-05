5dee4e33eb36e7bbd041d3431f8a0033e2341798
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-05 00:49:45 +0000

    Adding numeric_owner as keyword arguement

--- tools/buildtools/download_from_google_storage.py
+++ tools/buildtools/download_from_google_storage.py
@@ -343,7 +343,7 @@ def _downloader_worker_thread(thread_num, q, force, base_url,
                 if not is_within_directory(path, member_path):
                     raise Exception("Attempted Path Traversal in Tar File")
         
-            tar.extractall(path, members, numeric_owner) 
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
             
         
         safe_extract(tar, path=dirname)
