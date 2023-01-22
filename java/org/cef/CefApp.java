93aea8d74454a44febc6296e2739931351cf5872
ds58 <30220598+ds58@users.noreply.github.com>
2023-01-22 13:41:19 -0600

    Delete doMessageLoopWork implementation, delete CefRenderer

--- java/org/cef/CefApp.java
+++ java/org/cef/CefApp.java
@@ -430,49 +430,7 @@ public class CefApp extends CefAppHandlerAdapter {
      * Windows with windowed rendering.
      */
     public final void doMessageLoopWork(final long delay_ms) {
-        // Execute on the AWT event dispatching thread.
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                if (getState() == CefAppState.TERMINATED) return;
-
-                // The maximum number of milliseconds we're willing to wait between
-                // calls to DoMessageLoopWork().
-                final long kMaxTimerDelay = 1000 / 30; // 30fps
-
-                if (workTimer_ != null) {
-                    workTimer_.stop();
-                    workTimer_ = null;
-                }
-
-                if (delay_ms <= 0) {
-                    // Execute the work immediately.
-                    N_DoMessageLoopWork();
-
-                    // Schedule more work later.
-                    doMessageLoopWork(kMaxTimerDelay);
-                } else {
-                    long timer_delay_ms = delay_ms;
-                    // Never wait longer than the maximum allowed time.
-                    if (timer_delay_ms > kMaxTimerDelay) timer_delay_ms = kMaxTimerDelay;
-
-                    workTimer_ = new Timer((int) timer_delay_ms, new ActionListener() {
-                        @Override
-                        public void actionPerformed(ActionEvent evt) {
-                            // Timer has timed out.
-                            workTimer_.stop();
-                            workTimer_ = null;
-
-                            N_DoMessageLoopWork();
-
-                            // Schedule more work later.
-                            doMessageLoopWork(kMaxTimerDelay);
-                        }
-                    });
-                    workTimer_.start();
-                }
-            }
-        });
+        // Do nothing, handled by custom game tick loop
     }
 
     /**
