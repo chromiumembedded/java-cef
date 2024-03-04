e75a3c050cc6891fabce9a3918a6c06558a0c045
Rene Schneider <slartie@posteo.de>
2024-03-04 18:43:25 +0000

    Update to CEF 121.3.9+g1e0a38f+chromium-121.0.6167.184
    Added root_cache_path setting and onAlreadyRunningAppRelaunch event
    to deal with new CEF restrictions regarding parallel use of root_cache_path
    dir starting with CEF 120.

--- java/org/cef/handler/CefAppHandler.java
+++ java/org/cef/handler/CefAppHandler.java
@@ -72,4 +72,23 @@ public interface CefAppHandler {
      * currently pending scheduled call should be cancelled.
      */
     public void onScheduleMessagePumpWork(long delay_ms);
+
+    /**
+     * Implement this method to provide app-specific behavior when an already
+     * running app is relaunched with the same CefSettings.root_cache_path value.
+     * For example, activate an existing app window or create a new app window.
+     * |command_line| will be read-only. Do not keep a reference to
+     * |command_line| outside of this method. Return true if the relaunch is
+     * handled or false for default relaunch behavior. Default behavior will
+     * create a new default styled Chrome window.
+     *
+     * To avoid cache corruption only a single app instance is allowed to run for
+     * a given CefSettings.root_cache_path value. On relaunch the app checks a
+     * process singleton lock and then forwards the new launch arguments to the
+     * already running app process before exiting early. Client apps should
+     * therefore check the CefInitialize() return value for early exit before
+     * proceeding.
+     */
+    public boolean onAlreadyRunningAppRelaunch(
+            CefCommandLine command_line, String current_directory);
 }
